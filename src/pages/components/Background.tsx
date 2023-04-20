deff0e51ad3c48bcf8ec8f47bcc5e7ee1873db33
Ramsey <ramseyv2@illinois.edu>
2023-04-19 19:26:47 -0500

    syle changes

--- src/pages/components/Background.tsx
+++ src/pages/components/Background.tsx
@@ -7,21 +7,21 @@ const settings = {
     useBouncyWalls: false,
   },
   particle: {
-    particleCount: 35,
+    particleCount: 30,
     color: "#ffffff",
-    minSize: 1,
+    minSize: .1,
     maxSize: 3,
   },
   velocity: {
     directionAngle: 0,
     directionAngleVariance: 360,
     minSpeed: .1,
-    maxSpeed: .8,
+    maxSpeed: .5,
   },
   opacity: {
     minOpacity: 0,
     maxOpacity: 0.7,
-    opacityTransitionTime: 3000,
+    opacityTransitionTime: 15000,
   },
 };
 
