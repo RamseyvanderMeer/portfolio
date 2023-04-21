e3d063dafaf91c121454863ae47b716772568d41
Ramsey <ramseyv2@illinois.edu>
2023-04-21 18:24:00 -0500

    contact in tailwind

--- src/pages/components/Contact.tsx
+++ src/pages/components/Contact.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { useState } from "react";
 import axios from "axios";
-import contact from "../styles/Contact.module.scss";
 interface HomeProps {
   setCurrent: (current: number) => void;
 }
-export default function Contact({setCurrent} : HomeProps) {
+export default function Contact({ setCurrent }: HomeProps) {
   const [message, setMessage] = useState("");
   const [subject, setSubject] = useState("");
   const [sender, setSender] = useState("");
@@ -22,14 +21,14 @@ export default function Contact({setCurrent} : HomeProps) {
       setSubject("");
       setSender("");
     }
-      alert("Message Sent");
-      const nextURL = "/";
-      const nextTitle = "Home Page";
-      const nextState = { additionalInformation: "Updated the URL with JS" };
+    alert("Message Sent");
+    const nextURL = "/";
+    const nextTitle = "Home Page";
+    const nextState = { additionalInformation: "Updated the URL with JS" };
 
-      // This will create a new entry in the browser's history, without reloading
-      window.history.pushState(nextState, nextTitle, nextURL);
-      setCurrent(0);
+    // This will create a new entry in the browser's history, without reloading
+    window.history.pushState(nextState, nextTitle, nextURL);
+    setCurrent(0);
   };
 
   const ClearForm = (e: React.FormEvent<HTMLFormElement>) => {
@@ -40,45 +39,72 @@ export default function Contact({setCurrent} : HomeProps) {
   };
 
   return (
-    <div className={contact.container}>
-      <form onSubmit={SendMail} onReset={ClearForm} className={contact.form}>
-        <h1 className={contact.title}>Contact</h1>
-        <label className={contact.label} htmlFor="sender">
+    <div className="z-1 absolute flex h-screen w-screen flex-col items-center justify-center text-white">
+      <form
+        onSubmit={SendMail}
+        onReset={ClearForm}
+        className="items-left justify-left align-left md:max-w-36 relative m-10 flex w-11/12 flex-col rounded-2xl border-2 border-solid border-white px-6 py-6 opacity-90 md:w-4/12 md:px-20 md:py-6"
+      >
+        <h1 className="relative -left-2 mb-4 w-full text-left text-6xl font-bold opacity-100">
+          Contact
+        </h1>
+        <label
+          className="m-0 w-full text-left text-base font-semibold"
+          htmlFor="sender"
+        >
           Contact Email:
         </label>
         <input
           type="email"
           required
           value={sender}
-          className={contact.input}
+          className="mx-0 my-2 rounded-sm border-b border-white bg-transparent p-4 text-lg outline-none"
           onChange={(e) => setSender(e.target.value)}
         ></input>
-        <label className={contact.label} htmlFor="subject">
+        <label
+          className="m-0 w-full text-left text-base font-semibold"
+          htmlFor="subject"
+        >
           Subject:
         </label>
         <input
           type="subject"
           required
           value={subject}
-          className={contact.input}
+          className="mx-0 my-2 rounded-sm border-b border-white bg-transparent p-4 text-lg outline-none"
           onChange={(e) => setSubject(e.target.value)}
         ></input>
-        <label className={contact.label} htmlFor="message">
+        <label
+          className="m-0 w-full text-left text-base font-semibold"
+          htmlFor="message"
+        >
           Message:
         </label>
         <textarea
           required
           value={message}
-          className={contact.input}
+          className="mx-0 my-2 rounded-sm border-b border-white bg-transparent p-4 text-lg outline-none"
           rows={4}
           cols={20}
           onChange={(e) => setMessage(e.target.value)}
         ></textarea>
-        <div className={contact.buttonContainer}>
-          <button className={contact.clear} type="reset">
+
+        {/*
+cursor: pointer;
+  background-color: white;
+  color: black; */}
+
+        <div className="align-center my-4 flex w-full flex-row justify-between">
+          <button
+            className="mx-0 my-2 rounded-3xl border border-white bg-black px-6 py-2 transition duration-300 ease-in-out hover:bg-white hover:text-black"
+            type="reset"
+          >
             Clear
           </button>
-          <button className={contact.submit} type="submit">
+          <button
+            className="mx-0 my-2 rounded-3xl border border-white bg-black px-6 py-2 transition duration-300 ease-in-out hover:bg-white hover:text-black"
+            type="submit"
+          >
             Send
           </button>
         </div>
